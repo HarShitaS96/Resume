b1eca83f9bf660ab3ca38f27f2178340868591d9
HarShitaS96 <94897683+HarShitaS96@users.noreply.github.com>
2026-06-09 02:26:07 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
+const startDate = new Date(2019, 9, 7);
 
+const today = new Date();
+
+let years = today.getFullYear() - startDate.getFullYear();
+let months = today.getMonth() - startDate.getMonth();
+let days = today.getDate() - startDate.getDate();
+
+if (days < 0) {
+    months--;
+    const previousMonth = new Date(
+        today.getFullYear(),
+        today.getMonth(),
+        0
+    );
+
+    days += previousMonth.getDate();
+}
+
+if (months < 0) {
+    years--;
+    months += 12;
+}
+
+document.getElementById("experience").innerHTML =
+    years + " Years " +
+    months + " Months " +
+    days + " Days";
